abc687cd33a043b4eb1dafcd44fd163c44edb1c9
SimoneLollini <lollinisimone@gmail.com>
2022-12-19 16:54:42 +0100

    Add css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,11 +2,15 @@
 
 @section ('content')
 <div class="container">
-    <div class="row gy-5 my-5 row-cols-md-3">
+    <div class="row gy-5 my-5 row-cols-md-3 justify-content-center">
         @forelse ($movies as $movie)
         <div class="col">
-            <div class="card h-100">
+            <div class="card text-center p-2 h-100">
                 <h3>{{$movie->title}}</h3>
+                <div>{{$movie->original_title}}</div>
+                <div>{{$movie->nationality}}</div>
+                <div>{{$movie->date}}</div>
+                <div>{{$movie->vote}}</div>
             </div>
         </div>
         @empty
