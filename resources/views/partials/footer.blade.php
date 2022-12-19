665ff09f1cf608d535a86f7925c553b5a1a7aa8a
SimoneLollini <lollinisimone@gmail.com>
2022-12-19 15:47:12 +0100

    add PageController

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,13 @@
+<footer class="bg-dark">
+    <div class="container">
+        <a href="#" class="text-decoration-none text-secondary d-block">Lorem, ipsum dolor.</a>
+        <a href="#" class="text-decoration-none text-secondary d-block">Lorem, ipsum dolor.</a>
+        <a href="#" class="text-decoration-none text-secondary d-block">Lorem, ipsum dolor.</a>
+        <a href="#" class="text-decoration-none text-secondary d-block">Lorem, ipsum dolor.</a>
+        <a href="#" class="text-decoration-none text-secondary d-block">Lorem, ipsum dolor.</a>
+        <a href="#" class="text-decoration-none text-secondary d-block">Lorem, ipsum dolor.</a>
+        <a href="#" class="text-decoration-none text-secondary d-block">Lorem, ipsum dolor.</a>
+        <a href="#" class="text-decoration-none text-secondary d-block">Lorem, ipsum dolor.</a>
+        <a href="#" class="text-decoration-none text-secondary d-block">Lorem, ipsum dolor.</a>
+    </div>
+</footer>
